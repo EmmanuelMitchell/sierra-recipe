8fce4f460877a8f1bc5ddfcbe7e0617f0189850a
EmmanuelMitchell <97774003+EmmanuelMitchell@users.noreply.github.com>
2025-04-14 20:33:17 +0000

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,2 @@
-console.log("hello world i write the backend when i have time");
-console.log("hello world i write the backend when i have time");
+console.log("hello world i will write the backend when i have time");
+console.log("hello world i will write the backend when i have time");
